18fd676899b7ac8eec6229cb6487bba77caafec9
Matthew Hanson <matt.a.hanson@gmail.com>
2022-10-08 23:42:25 -0400

    gracefully close the event loop

--- stactask/task.py
+++ stactask/task.py
@@ -286,3 +286,23 @@ class Task(ABC):
                     item_collection = json.loads(f.read())
             # run task handler
             cls.handler(item_collection, **args)
+
+
+# from https://pythonalgos.com/runtimeerror-event-loop-is-closed-asyncio-fix/
+"""fix yelling at me error"""
+from functools import wraps
+ 
+from asyncio.proactor_events import _ProactorBasePipeTransport
+ 
+def silence_event_loop_closed(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except RuntimeError as e:
+            if str(e) != 'Event loop is closed':
+                raise
+    return wrapper
+ 
+_ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
+"""fix yelling at me error end"""
