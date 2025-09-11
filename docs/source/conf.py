9e106e1d0879d32eff03b6421b4ada9ed7532a0d
pjhartzell <preston.hartzell@gmail.com>
2025-09-11 18:23:51 -0500

    possible readthedocs fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,7 @@
 import os
 import sys
 
-sys.path.insert(
-    0, os.path.abspath("../../src")
-)  # Source code dir relative to this file
+sys.path.insert(0, os.path.abspath("../.."))  # Source code dir relative to this file
 
 # -- Project information
 
